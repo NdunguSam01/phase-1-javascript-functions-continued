ba9581d3ace9cabe0b525ae6acca780a631706da
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-05 13:28:19 +0300

    Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const saturdayFun=(activity="roller-skate")=>
+{
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun())
+console.log(saturdayFun("clean the house"))
+
+const mondayWork=(activity="go to the office")=>
+{
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork())
+console.log(mondayWork("work from home"))
+
+const wrapAdjective=(symbol)=>
+{
+    const returnMessage=(emphatic)=>
+    {
+        return `You are ${symbol}${emphatic}${symbol}!`
+    }
+    return returnMessage
+}
+console.log(wrapAdjective("%")("a dedicated programmer"))
